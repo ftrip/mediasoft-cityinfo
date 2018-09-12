ef3d9d7673f22881db1fef7d5544ba819b300a9f
Sergey <sinbir@ftrip.ru>
2018-09-12 10:25:29 +0300

    begin

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from 'Components/App';
+import reducer from './store/reducers';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('react-root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    publicPath: '/',
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.scss'],
+    alias: {
+      Components: path.resolve(__dirname, 'src', 'components'),
+      Containers: path.resolve(__dirname, 'src', 'containers'),
+      Pages: path.resolve(__dirname, 'src', 'pages'),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist'),
+    new HtmlWebpackPlugin({ template: path.join(__dirname, 'src', 'index.html') }),
+    new MiniCSSExtractPlugin({ filename: 'style.css' }),
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
+};
